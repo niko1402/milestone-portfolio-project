096fefb64081e0d15243bd83f4256c3c0ace9f65
nik_e_p <nikpayton@gmail.com>
2022-12-04 10:58:41 -0300

    Put header divider back in for mobile only. Added new hamburger code in js. Changed header h3 to 1.75 for screens above 600px, and text-align left. Changed nav.menu to top 115, and gap 0px. Lowered menu-items to margin 10px

--- js/main.js
+++ js/main.js
@@ -29,3 +29,16 @@ window.addEventListener("scroll", e => {
 });
 
 // Hamburger Menu
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
